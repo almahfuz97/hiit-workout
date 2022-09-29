b087b3d7401a42ab56c426929d214e505f8f7687
almahfuz <almahfuz0327@gmail.com>
2022-09-29 13:08:55 +0600

    added react toastify

--- src/components/Details.js
+++ src/components/Details.js
@@ -2,21 +2,31 @@ import React from "react";
 import BreakTime from "./BreakTime";
 import ExerciseDetails from "./ExerciseDetails";
 import UserProfile from "./UserProfile";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Details({
   exerciseTime,
   handleBreakTimeClick,
   breakTime,
 }) {
+  // toast
+  const showToast = () => {
+    toast("Congratulations! You have completed your workout");
+  };
   return (
     <div className="w-full  h-full bg-white ">
       <UserProfile />
       <BreakTime handleBreakTimeClick={handleBreakTimeClick} />
       <ExerciseDetails exerciseTime={exerciseTime} breakTime={breakTime} />
-      <button className=" border p-2 mt-5  rounded ">
+      <button
+        onClick={showToast}
+        className="border bg-white shadow-md shadow-pink-300 justify-center px-2 py-2 inline-block text-xs text-pink-500 mt-4 bg-transparent rounded-md hover:bg-slate-50"
+      >
         {" "}
         Activity Completed
       </button>
+      <ToastContainer />
     </div>
   );
 }
